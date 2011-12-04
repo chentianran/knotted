abf388d4240859934a83ef7380d3293af290d523
Tianran Chen <tianran.chen@gmail.com>
2011-12-03 20:14:39 -0500

    Added homgen.py for generating complices from permutation rules

---
+++ homgen.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+
+import sys
+import string
+import itertools
+
+groups = []
+rules = {}
+
+for line in sys.stdin.readlines():
+    lst = line.split()
+    if lst:
+        if 'group' == lst[0].strip():
+            groups.append (lst[1:])
+        elif 'to' == lst[1]:
+            if len(lst) > 4 and 'with' == lst[3]:
+                rules[lst[0]] = (lst[2], lst[4])
+            else:
+                rules[lst[0]] = (lst[2], 1)
+        else:
+            print 'Sorry, I did not understand the command:'
+            print line
+
+generators = []
+for g in groups:
+    if not generators:
+        generators = g
+    else:
+        new_gen = []
+        for p, x in itertools.product (generators, g):
+            new_p = list(p)
+            new_p.append(x)
+            new_gen.append (new_p)
+        generators = new_gen
+
+for g in generators:
+    terms = []
+    for i in range(0,len(g)):
+        if g[i] in rules:
+            rule = rules[g[i]]
+            dst = list(g)
+            dst[i] = rule[0]
+            if 1 == rule[1]:
+                terms.append (''.join(dst))
+            else:
+                terms.append (rule[1] + '*' + ''.join(dst))
+    if terms:
+        print ''.join(g), '~', ' + '.join(terms)
+
