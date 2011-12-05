a75625803c44a07098da2c3d7d2dc0feae5f3f15
Tianran Chen <tianran.chen@gmail.com>
2011-12-05 17:45:09 -0500

    Improved homgen.py

--- homgen.py
+++ homgen.py
@@ -14,9 +14,13 @@ for line in sys.stdin.readlines():
             groups.append (lst[1:])
         elif 'to' == lst[1]:
             if len(lst) > 4 and 'with' == lst[3]:
-                rules[lst[0]] = (lst[2], lst[4])
+		coef = lst[4]
             else:
-                rules[lst[0]] = (lst[2], 1)
+		coef = 1
+	    if lst[0] in rules:
+		rules[lst[0]].append( (lst[2], coef) )
+	    else:
+		rules[lst[0]] = [ (lst[2], coef) ]
         else:
             print 'Sorry, I did not understand the command:'
             print line
@@ -37,13 +41,13 @@ for g in generators:
     terms = []
     for i in range(0,len(g)):
         if g[i] in rules:
-            rule = rules[g[i]]
-            dst = list(g)
-            dst[i] = rule[0]
-            if 1 == rule[1]:
-                terms.append (''.join(dst))
-            else:
-                terms.append (rule[1] + '*' + ''.join(dst))
+            for rule in rules[g[i]]:
+		dst = list(g)
+		dst[i] = rule[0]
+		if 1 == rule[1]:
+		    terms.append (''.join(dst))
+		else:
+		    terms.append (rule[1] + '*' + ''.join(dst))
     if terms:
         print ''.join(g), '~', ' + '.join(terms)
 
